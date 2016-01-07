282d1cb902c8f411513de9b110baf51875b270d8
Karla Morales <rc.moralesmarin.karla@leadps.org>
2016-01-07 14:28:55 -0800

    drawing a cross

---
+++ KarlaMorales/drawTee.py
@@ -0,0 +1,17 @@
+import turtle
+
+def drawTee(myTurtle):
+	myTurtle.forward(200)
+	myTurtle.backward(50)
+	myTurtle.right(90)
+	myTurtle.forward(50)
+	myTurtle.backward(100)
+
+def drawFourTees(myTurtle):
+	pass 
+
+shawn = turtle.Turtle() 
+
+drawTee(shawn)
+
+# make your turtle down here and psss it to the appropriate places
