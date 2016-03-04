31656a78ccff211609bbb7f39b714268e6abfda8
Karla Morales <rc.moralesmarin.karla@leadps.org>
2016-03-04 15:33:46 -0800

    ps6

--- teamManagerDeluxe.py
+++ teamManagerDeluxe.py
@@ -24,6 +24,43 @@ print("Enter the number of your choice and press enter.")
 print("(1) Start with a new team")
 print("(2) Open a file for an existing team")
 
+# takes the playerList and the user's desired filename
+# writes each Player to file
+def saveTeam(playerList, filename):
+	file = open(filename, 'w')
+ 
+	# write to the file
+	for p in playerList:
+		file.write(p.player_name + " " + str(p.player_age) + " " + str(p.goals) + " " + str(p.jersey_num) + " " + p.position + "\n")   
+	# close the file
+ 	file.close()
+
+ 
+# takes a filename for a file of players
+# returns a list of Players
+def loadTeam(filename):
+ 
+    # create empty list
+	team = []
+    # open the file
+	file = open(filename, 'r')
+    # read each line and create Player from it (use a loop)
+	line = file.readline()
+	while line != " ": 
+		my_player = my_line.split()
+		team.append(Player(name, age, goals, jersey_num, position))
+		print(my_player[0]) 	
+        # split each line into a list of the variables
+		line = file.readline()
+        # read each next line
+ 
+    # close the file
+		file.close()		
+    # return the completed list
+ 
+    # placeholder - delete once the function is complete
+	pass
+
 
 choice = int(raw_input())
 
@@ -41,15 +78,34 @@ while choice != 0:
 		print("What is your players position?")
 		position = str(raw_input())
 		my_players.append(Player(name, age, goals, jersey_num, position))
-		options = int(raw_input())
+		
+		print("choose an option")
+                print("option 1 is to add players to the team")
+                print("option 2 is to view the players")
+                print("option 3 is to save the team to a file")
 
-	elif options == 2:
+		choice = int(raw_input())
+
+	elif choice == 2:
 		for p in my_players:
 			p.showPlayer()
 		print("choose an option")
 		print("option 1 is to add players to the team")
 		print("option 2 is to view the players")
-		option = int(raw_input())
+		print("option 3 is to save the team to a file")
+		choice = int(raw_input())
+
+	elif choice == 3:
+		print("what do you want to call the file?")
+		filename = raw_input()	
+		saveTeam(my_players, filename)
+
+		print("choose an option")
+                print("option 1 is to add players to the team")
+                print("option 2 is to view the players")
+                print("option 3 is to save the team to a file")
+                choice = int(raw_input())
+	
 
 my_players.append(Player(name, age, goals))
 
